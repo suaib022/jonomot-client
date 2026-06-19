ac3fd10f7ffee06fad70f0298226a6a103a68272
suaib022 <suaib.109206@gmail.com>
2026-06-19 11:10:00 +0600

    setup react router for main navigation

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Home from '../pages/Home';
+import CreatePost from '../pages/CreatePost';
+import Community from '../pages/Community';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+import { PublicRoute } from './PublicRoute';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/create',
+        element: <CreatePost />,
+      },
+      {
+        path: '/j/:communityName',
+        element: <Community />,
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <PublicRoute><Login /></PublicRoute>,
+  },
+  {
+    path: '/register',
+    element: <PublicRoute><Register /></PublicRoute>,
+  },
+]);
+
+export default router;
