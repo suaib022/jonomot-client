11f5faf547b2a51a8b63cbf1901ad6312debcc1f
suaib022 <suaib.109206@gmail.com>
2026-06-25 10:22:00 +0600

    add api integration for communities

---
+++ src/redux/features/community/communityApi.ts
@@ -0,0 +1,71 @@
+import { baseApi } from '../../api/baseApi';
+
+export const communityApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createCommunity: builder.mutation({
+      query: (data) => ({
+        url: '/communities',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Community'],
+    }),
+    getAllCommunities: builder.query({
+      query: () => '/communities',
+      providesTags: ['Community'],
+    }),
+    getJoinedCommunities: builder.query({
+      query: () => '/communities/joined',
+      providesTags: ['Community'],
+    }),
+    getCommunityByName: builder.query({
+      query: (name) => `/communities/${name}`,
+      providesTags: (_result, _error, name) => [{ type: 'Community', id: name }],
+    }),
+    joinCommunity: builder.mutation({
+      query: (communityId) => ({
+        url: `/communities/${communityId}/join`,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Community'],
+    }),
+    leaveCommunity: builder.mutation({
+      query: (communityId) => ({
+        url: `/communities/${communityId}/leave`,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Community'],
+    }),
+    getCommunityMembers: builder.query({
+      query: (communityId) => `/communities/${communityId}/members`,
+      providesTags: (result, error, id) => [{ type: 'Community', id: `members-${id}` }],
+    }),
+    updateMemberRole: builder.mutation({
+      query: ({ communityId, userId, role }) => ({
+        url: `/communities/${communityId}/role`,
+        method: 'PATCH',
+        body: { user_id: userId, role },
+      }),
+      invalidatesTags: (result, error, { communityId }) => [{ type: 'Community', id: `members-${communityId}` }],
+    }),
+    removeMember: builder.mutation({
+      query: ({ communityId, userId }) => ({
+        url: `/communities/${communityId}/members/${userId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, { communityId }) => [{ type: 'Community', id: `members-${communityId}` }],
+    }),
+  }),
+});
+
+export const {
+  useCreateCommunityMutation,
+  useGetAllCommunitiesQuery,
+  useGetJoinedCommunitiesQuery,
+  useGetCommunityByNameQuery,
+  useJoinCommunityMutation,
+  useLeaveCommunityMutation,
+  useGetCommunityMembersQuery,
+  useUpdateMemberRoleMutation,
+  useRemoveMemberMutation,
+} = communityApi;
